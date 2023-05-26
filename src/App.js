c8223e94f6136895534d01df26123304ec2789b2
soumen9775 <rakshitsoumen4@gmail.com>
2023-05-26 23:13:03 +0530

    1st cmmit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Resturant } from './component/Basic/Resturant';
+//import { UseState } from './component/Hooks/UseState';
+// import { UseEffect } from './component/Hooks/UseEffect';
+// import {UseReducer} from './component/Hooks/UseReducer';
+export const App = () => {
+  return (
+    <div>
+      <Resturant />
+      {/* <UseState/> */}
+      {/* <UseEffect/> */}
+      {/* <UseReducer/> */}
+    </div>
+  )
+}
+export default App;
